ca32211d24525089ec413440d41696dbbefc58a4
uwemerayesu <45234953+uwemerayesu@users.noreply.github.com>
2018-12-04 13:12:55 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+namespace Class
+{
+    class Calculate
+    
+    {
+        public double Add(double num1,double num2)
+        {
+    return (num1 + num1);
+        }
+        public double substract( double num1,double num2)
+        {
+ return(num1 - num2);
+        }
+        public double divide( double num1, double num2)
+        {
+            if(num2 !=0)
+            return(num1/num2);
+            else
+            {
+              throw new Exception("imposible");  
+            }
+        }
+        public double multiply( double num1, double num2)
+        {
+return(num1*num2);
+        }
+    }
+}
