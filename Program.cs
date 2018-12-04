ddcaf44897706f5998fc15fff8c20ff6e970bb49
uwemerayesu <45234953+uwemerayesu@users.noreply.github.com>
2018-12-04 14:56:33 +0200

    first commit

--- Program.cs
+++ Program.cs
@@ -1,29 +1,62 @@
-﻿using System; 
-namespace Class
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
 {
-    class Calculate
-    
+    class Program
     {
-        public double Add(double num1,double num2)
+        
+        static void Main(string[] args)
         {
-    return (num1 + num1);
-        }
-        public double substract( double num1,double num2)
-        {
- return(num1 - num2);
-        }
-        public double divide( double num1, double num2)
-        {
-            if(num2 !=0)
-            return(num1/num2);
+            int c;
+               var operation1 = new calculate();
+               Console.WriteLine("enter first number");
+               double num1 = Convert.ToDouble(Console.ReadLine());
+               Console.WriteLine("enter second number");
+               double num2 = Convert.ToDouble(Console.ReadLine());
+               Console.Write("choose any operator continue:\n\n");
+            Console.WriteLine("1. addition");
+            Console.WriteLine("2. substract");
+            Console.WriteLine("3. Divide");
+            Console.WriteLine("4. Multiply\n\n\n\n\n");
+            int C = Convert.ToInt16(Console.ReadLine());
+            
+            if (C == 1)
+            {
+                operation1.Add(num1, num2);
+                Console.WriteLine("the result of :{0} and {1}is equal to {2}", num1, num2, operation1.Add(num1, num2));
+            }
+  else if( C==2)
+            {
+                operation1.Substract(num1, num2);
+                Console.WriteLine("the result of :{0} and {1}is equal to {2}", num1, num2, operation1.Substract(num1, num2));
+            }
+            else if (C == 3)
+            {
+                operation1.Divide(num1, num2);
+                Console.WriteLine("the result of :{0} and {1}is equal to {2}", num1, num2, operation1.Divide(num1, num2));
+            }
+            else if (C == 4)
+            {
+                operation1.Multiply(num1, num2);
+                Console.WriteLine("the result of : {0} and {1}is equal to {2}", num1, num2, operation1.Multiply(num1, num2));
+            }
             else
             {
-              throw new Exception("imposible");  
+                Console.WriteLine("impossible calculation");
             }
-        }
-        public double multiply( double num1, double num2)
-        {
-return(num1*num2);
-        }
+            Console.ReadKey();
+               
+
+   
+          
+         }
+
+
+
+
     }
 }
---
+++ calculate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class calculate
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Substract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new Exception("num 2 should not be equal to zero");
+            }
+            return num1 / num2;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
